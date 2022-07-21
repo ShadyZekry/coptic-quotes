4ba25a77f7b32c4bbe600ff88299ccb36c258e5c
Shady Zekry <shadyzekry75@gmail.com>
2022-07-22 01:20:14 +0200

    Sort db values in seeder

--- database/seeders/FillWithDefaultValues.php
+++ database/seeders/FillWithDefaultValues.php
@@ -18,23 +18,23 @@ class FillWithDefaultValues extends Seeder
     {
         Author::insert(
             [
+                ['name' => 'أبونا بيشوى كامل', 'created_at' => now(), 'updated_at' => now()],
                 ['name' => 'الأنبا أنطونيوس', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'الأنبا موسى الأسود', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'الأنبا بولا أول السواح', 'created_at' => now(), 'updated_at' => now()],
                 ['name' => 'الأنبا باخوميوس أب الشركة', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'القديس أغسطينوس', 'created_at' => now(), 'updated_at' => now()],
+                ['name' => 'الأنبا بولا أول السواح', 'created_at' => now(), 'updated_at' => now()],
                 ['name' => 'الأنبا شنودة رئيس المتوحدين', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'الشيخ الروحانى (القديس يوحنا سابا)', 'created_at' => now(), 'updated_at' => now()],
+                ['name' => 'الأنبا مكاريوس الكبير', 'created_at' => now(), 'updated_at' => now()],
+                ['name' => 'الأنبا موسى الأسود', 'created_at' => now(), 'updated_at' => now()],
+                ['name' => 'البابا أثناسيوس الرسولى', 'created_at' => now(), 'updated_at' => now()],
                 ['name' => 'البابا شنودة الثالث', 'created_at' => now(), 'updated_at' => now()],
                 ['name' => 'البابا كيرلس السادس', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'أبونا بيشوى كامل', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'القديس ماراسحق السريانى', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'البابا أثناسيوس الرسولى', 'created_at' => now(), 'updated_at' => now()],
+                ['name' => 'الشهيد الأنبا كبريانوس أسقف قرطاجنة', 'created_at' => now(), 'updated_at' => now()],
+                ['name' => 'الشيخ الروحانى (القديس يوحنا سابا)', 'created_at' => now(), 'updated_at' => now()],
+                ['name' => 'القديس أغسطينوس', 'created_at' => now(), 'updated_at' => now()],
                 ['name' => 'القديس إكليمندس الإسكندرى', 'created_at' => now(), 'updated_at' => now()],
                 ['name' => 'القديس ثيوفان الناسك', 'created_at' => now(), 'updated_at' => now()],
+                ['name' => 'القديس ماراسحق السريانى', 'created_at' => now(), 'updated_at' => now()],
                 ['name' => 'القديس مارافرام السريانى', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'الشهيد الأنبا كبريانوس أسقف قرطاجنة', 'created_at' => now(), 'updated_at' => now()],
-                ['name' => 'الأنبا مكاريوس الكبير', 'created_at' => now(), 'updated_at' => now()],
             ]
         );
 
