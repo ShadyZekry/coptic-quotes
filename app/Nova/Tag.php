227178087bb2c87a09999f4e848451a0a02c52ba
Shady Zekry <shadyzekry75@gmail.com>
2022-08-05 00:07:07 +0200

    Add quote/tag resource relationship

--- app/Nova/Tag.php
+++ app/Nova/Tag.php
@@ -2,35 +2,34 @@
 
 namespace App\Nova;
 
-use Laravel\Nova\Fields\BelongsTo;
+use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsToMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
-use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
-class Quote extends Resource
+class Tag extends Resource
 {
     /**
      * The model the resource corresponds to.
      *
      * @var string
      */
-    public static $model = \App\Models\Quote::class;
+    public static $model = \App\Models\Tag::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
      *
      * @var string
      */
-    public static $title = 'quote';
+    public static $title = 'name';
 
     /**
      * The columns that should be searched.
      *
      * @var array
      */
-    public static $search = ['id', 'quote'];
+    public static $search = ['id', 'name'];
 
     /**
      * Get the fields displayed by the resource.
@@ -42,11 +41,8 @@ class Quote extends Resource
     {
         return [
             ID::make()->sortable(),
-            BelongsTo::make('Author')->required()->sortable(),
-            Text::make('Quote')->displayUsing(fn ($value) => mb_strimwidth($value, 0, 100, '...'))->onlyOnIndex()->sortable(),
-            Textarea::make('Quote')->required(),
-            BelongsTo::make('Source')->required()->sortable(),
-            BelongsToMany::make('Tags')->required()->sortable(),
+            Text::make('name')->sortable(),
+            BelongsToMany::make('Quotes')->required()->sortable(),
         ];
     }
 
